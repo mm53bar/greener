a5fd4e10bcc23de388194f1b8b84c3c5725a523d
Darko Dosenovic <darko.dosenovic@ama.ab.ca>
2012-02-16 11:14:49 -0700

    Initial commit, via template generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenApple::Application.config.secret_token = '74e65e2b68291cb8e3b85692c7cd7b0d9480eb61ff288dbe8a8094cba8f5837582ce6aff61015220f6d0aca097288bcbbec13d93b9e40f3f57600d8c966199d4'
